97e37ba584e0a957c21ed368d6012e201b284721
nancyhd <dougherty.nancyh@gmail.com>
2013-12-27 21:53:05 -0800

    SPI driver first pass complete

--- Firmware/lib/ADXL362.c
+++ Firmware/lib/ADXL362.c
@@ -20,64 +20,100 @@
 
 /*Macros*/
 //Processor Interrupt Configuration
-#define configure_pin_ss() 			IODIR0 |= (1<<P0_20_SHIFT) /*Sets the CS pin, P0.20, to output*/
-#define configure_pin_irq() 		PINSEL1 |= ((EINT_SELECT) << (P0_30_SHIFT)) /*sets P0.30 to EINT3*/
-#define configure_irq_direction()	EXTMODE |= 0x04; EXTPOLAR |= 0x04  /*sets the interrupt to edge sensitive, rising edge*/
-#define configure_int_wakeup()		INTWAKE |= (1<<3) /*so EINT3 can wake the processor from sleep*/
+#define configure_pin_ss() 			IODIR0       |= (1<<P0_20_SHIFT) /*Sets the CS pin, P0.20, to output*/
+#define configure_pin_irq() 		PINSEL1      |= ((EINT_SELECT) << (P0_30_SHIFT)) /*sets P0.30 to EINT3*/
+#define configure_irq_direction()	EXTMODE      |= 0x04; EXTPOLAR |= 0x04  /*sets the interrupt to edge sensitive, rising edge*/
+#define configure_int_wakeup()		INTWAKE      |= (1<<3) /*so EINT3 can wake the processor from sleep*/
 #define configure_VIC_int()			VICIntEnable |= (1<<17) /*17 is the EINT3 wakeup*/
 //clear interrupt: write 1 to EXTINT, clear VicIntEnClr
 
-//Accelerometer configuration
-#define configure_fifo_en()			XL362_FIFO_CONTROL |= (XL362_FIFO_MODE_STREAM | XL362_FIFO_SAMPLES_AH)
-#define configure_int1_watermark()	XL362_INTMAP1 |= XL362_INT_FIFO_WATERMARK
-#define configure_sampling()		XL362_FILTER_CTL |= XL362_RATE_400  /*2G is default*/
-#define configure_noise()			XL362_POWER_CTL |= XL362_LOW_NOISE2
-#define enable_meas()				XL362_POWER_CTL |= XL362_MEASURE_3D
-#define configure_ext_clk()			XL362_POWER_CTL |= XL362_EXT_CLK
-#define configure_trig_sampling()	XL362_FILTER_CTL |= XL362_EXT_TRIGGER
-#define configure_other_ints()		XL362_THRESH_ACT_H |= 0x04; XL362_TIME_ACT |= 0xFF
 
 //SPI CS macros
-#define select_ADXL362() 		IOCLR0 |= (1<<P0_20_SHIFT)
-#define unselect_ADXL362() 		IOSET0 |= (1<<P0_20_SHIFT)
+#define select_ADXL362() 			IOCLR0 |= (1<<P0_20_SHIFT)
+#define deselect_ADXL362() 			IOSET0 |= (1<<P0_20_SHIFT)
+
+//debug pin
+#define configure_debug_pin()		IODIR0 |= (1<<12) /*P0.12 (P8 on the board) is my debug pin*/
+#define set_debug()					IOSET0 |= (1<<12)
+#define clr_debug()					IOCLR0 |= (1<<12)
 
 
 void ADXL362_Init(void){
+	/*configure processor for the XL interrupt*/
 	configure_pin_ss();
 	configure_pin_irq();
 	configure_irq_direction();
 	configure_int_wakeup();
 	configure_VIC_int();
-	configure_fifo_en(); /*enables in stream mode with 384 sample deep FIFO (128 in 3 axis)*/
-	configure_fifo_samples();
-	configure_int1_watermark();
-	configure_sampling();
-	configure_noise();
-	configure_other_ints(); //sets activity, motion thresholds high so they don't trigger as often; ints ignored
-	enable_meas();
-	//configure_trig_sampling();	//wire trigger to int2
+	configure_debug_pin(); //for debug use :)  SPI port = main debug port
+
+	/*configure accelerometer*/
+
+	//enables the FIFO in stream mode, 384 sample deep watermark (128 in 3 axes)
+	ConfigureAcc(XL362_FIFO_CTL, (XL362_FIFO_MODE_STREAM | XL362_FIFO_SAMPLES_AH));
+	//set int1 to a watermark interrupt (watermark configured in FILTER_CONTROL; 128samples of each axis)
+	ConfigureAcc(XL362_INTMAP1, XL362_INT_FIFO_WATERMARK);
+	//set the sampling rate to 400Hz
+	ConfigureAcc(XL362_FILTER_CTL, XL362_RATE_400);
+	//set into low noise mode
+	ConfigureAcc(XL362_POWER_CTL, XL362_LOW_NOISE2);
+	//Configure activity interrups to >0 so if enabled, they don't trigger constantly (still not enabled, though)
+	ConfigureAcc(XL362_THRESH_ACT_H, 0x04);
+	ConfigureAcc(XL362_TIME_ACT, 0xFF);
+	//Configure external trigger sampling
+	//ConfigureAcc(XL362_FILTER_CTL, XL362_EXT_TRIGGER);
+
+	//enable the accelerometer into measurement mode
+	ConfigureAcc(XL362_POWER_CTL, XL362_MEASURE_3D);
 
 }
 
-bool ADXLDeviceIDCheck() {
+
+//will only set low register values high; may need to add functionality later to set values to 0
+unsigned char ConfigureAcc(unsigned char reg, unsigned char value) {
+	unsigned char current_reg_val = ReadAcc(reg);
+	unsigned char new_val = current_reg_val |= value;
+	WriteAcc(reg, new_val);
+	return new_val;
+}
+
+unsigned char ReadAcc(unsigned char reg) {
+	select_ADXL362();
 	SPI1_Write(XL362_REG_READ);
-	SPI1_Write(XL362_DEVID_AD);
-	unsigned char deviceID = SPI1_Read();
-	return deviceID == 0xAD;
+	SPI1_Write(reg);
+	unsigned char read_val = SPI1_Read();
+	deselect_ADXL362();
+	return read_val;
+}
+
+void WriteAcc(unsigned char reg, unsigned char value) {
+	select_ADXL362();
+	SPI1_Write(XL362_REG_WRITE);
+	SPI1_Write(reg);
+	SPI1_Write(value);
+	deselect_ADXL362();
+	return;
 }
 
 
+bool ADXLDeviceIDCheck(void) {
+	unsigned char id = ReadAcc(XL362_DEVID_AD);
+	return (id == 0xAD);
+}
 
-int readNumSamplesFifo(){
 
-	SPI1_Write(XL362_REG_READ);
-	SPI1_Write(XL362_FIFO_ENTRIES_L);
-	unsigned char fifo_num_l = SPI1_Read();
-	unsigned char fifo_num_h = SPI1_Read() & 0x03; /*only the last 2 bits of the upper register matter*/
-
+int readNumSamplesFifo(void){
+	unsigned char fifo_num_l = ReadAcc(XL362_FIFO_ENTRIES_L);
+	unsigned char fifo_num_h = ReadAcc(XL362_FIFO_ENTRIES_H);
+	fifo_num_h &= 0x03; /*only the last 2 bits of the upper register matter*/
 	int fifo_num_samples = (int)(((int)fifo_num_h<<8) | fifo_num_l);
 	return fifo_num_samples;
 }
+
+void startADXL362(void) {
+	enable_meas();
+}
+
 
 
 
